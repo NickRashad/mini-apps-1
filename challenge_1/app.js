86f64003435d44c7d75ded56e8da513186fe4ab7
NickRashad <nrsternes@gmail.com>
2019-04-02 00:43:43 -0500

    Start TicTacToe app

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,12 @@
+console.log('Yep!');
 
+// Model
+// Player's X & O score & startfirst
+// Current Player Turn
 
+// View
+// Player's X & O display indicator and score
+// 3 X 3 Table
+
+// Controller
+//
