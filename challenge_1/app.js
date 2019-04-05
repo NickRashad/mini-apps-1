441af4870b0620b724616a61a554a49c5b24e964
NickRashad <nrsternes@gmail.com>
2019-04-05 13:58:18 -0500

    Fix css & stop game upon a winner

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,28 +1,29 @@
 console.log('Yep!');
 
 // Model
-// Player's X & O score & startfirst
 // Current Player Turn
-var currentTurn = true;
-var board = { 'X': [], 'O': [] };
-var totalWins = { 'X': 0, 'O': 0 };
-var swap = () => currentTurn = ! currentTurn;
-var move = () => currentTurn ? "X" : "O";
-var isWinner = (winner) => totalWins[winner] += 1;
-var canAdd = (position) => {
+let currentTurn = true;
+const move = () => currentTurn ? "X" : "O";
+const swap = () => currentTurn = ! currentTurn;
+
+// Keep track of board
+let board = { 'X': [], 'O': [] };
+const canAdd = (position) => {
   for (let curr in board) {
     if (board[curr].includes(position)) { return false; };
   }
   return true;
 };
-var isWin = (curr) => {
-  let winningMoves = [
-    [1,2,3], [4,5,6], [7,8,9],
-    [1,4,7], [2,5,8], [3,6,9],
-    [1,5,9], [3,5,7]
-  ];
-  for (let arr of winningMoves){
+// Keep track of wins
+let totalWins = { 'X': 0, 'O': 0 };
+let gameOn = true;
+const isWinner = (winner) => totalWins[winner] += 1;
+const isWin = (curr) => {
+  let winningMoves = [ [1,2,3], [4,5,6], [7,8,9],
+    [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ];
+  for (let arr of winningMoves) {
     if(arr.every(val => board[curr].includes(val))) {
+      gameOn = false;
       return true;
     };
   }
@@ -34,18 +35,16 @@ var isWin = (curr) => {
 
 
 // Controller
-var squares = document.querySelectorAll("th");
+let squares = document.querySelectorAll("th");
 squares.forEach((elem) => {
   elem.addEventListener("click", () => {
-    var curr = move();
+    let curr = move();
     let pos = Number(elem.className);
-    if (canAdd(pos)) {
+    if (canAdd(pos) && gameOn) {
       board[curr].push(pos);
       elem.innerHTML= curr;
       isWin(curr) ? isWinner(curr.toString()) : swap();
     }
   });
 });
-
-// console.log(squares);
 
