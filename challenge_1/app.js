ae00ae47b93fe8ad82db8e780a4f1d6d1d660605
NickRashad <nrsternes@gmail.com>
2019-04-05 16:14:41 -0500

    Complete tic-tac-toe functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,21 @@
-console.log('Yep!');
-
 // Model
 // Current Player Turn
 let currentTurn = true;
-const move = () => currentTurn ? "X" : "O";
-const swap = () => currentTurn = ! currentTurn;
-
+const move = () => currentTurn ? 0 : 1;
+const swap = () => {
+  currentTurn = ! currentTurn;
+  displayMsg();
+};
+let players = {
+  0: 'X',
+  1: 'O',
+};
 // Keep track of board
-let board = { 'X': [], 'O': [] };
+let board = { '0': [], '1': [] };
 const newBoard = () => {
-  for(let player in board) { board[player] = []; }
+  board = { '0': [], '1': [] }
+  gameOn = true;
+  displayMsg('new');
 };
 const canAdd = (position) => {
   for (let curr in board) {
@@ -20,28 +26,39 @@ const canAdd = (position) => {
 const displayMsg = (decision) => {
   var message = document.getElementById('UserMessage');
   if (decision === 'new') {
-    message.innerHTML = `New Game. It is ${move()}'s turn`;
+    message.innerHTML = `New Game. It is ${players[move()]}'s turn`;
   } else if (decision === 'tie') {
     message.innerHTML = 'Tie Game! Please start a new game if you wish to continue';
-  } else {
-    message.innerHTML = `Winner! Congratulations player ${decision}!
+  } else if (decision !== undefined) {
+    message.innerHTML = `Winner! Congratulations player ${players[decision]}!
     Please start a new game if you wish to continue`;
+  } else if (gameOn) {
+    message.innerHTML = `It is ${players[move()]}'s turn`;
   }
 };
 // Keep track of wins
-let totalWins = { 'X': 0, 'O': 0 };
+let totalWins = { '0': 0, '1': 0 };
 let gameOn = true;
-const isWinner = (winner) => totalWins[winner] += 1;
+const isWinner = (winner) => {
+  totalWins[winner] += 1;
+  document.getElementById('player0-score').innerHTML = totalWins[0];
+  document.getElementById('player1-score').innerHTML = totalWins[1];
+};
+
 const isWin = (curr) => {
   let winningMoves = [ [1,2,3], [4,5,6], [7,8,9],
     [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] ];
   for (let arr of winningMoves) {
     if (arr.every(val => board[curr].includes(val))) {
       gameOn = false;
+      console.log(typeof curr);
       displayMsg(curr);
       return true;
-    } else if (board['X'].length + board['O'].length === 9) {
+    } else if (board['0'].length + board['1'].length === 9) {
+      gameOn = false;
       displayMsg('tie');
+    } else {
+      displayMsg()
     }
   }
 };
@@ -59,14 +76,14 @@ squares.forEach((elem) => {
     let pos = Number(elem.className);
     if (canAdd(pos) && gameOn) {
       board[curr].push(pos);
-      elem.innerHTML= curr;
+      elem.innerHTML= curr ? 'O' : 'X';
       isWin(curr) ? isWinner(curr.toString()) : swap();
     }
   });
 });
+
 let newGame = document.getElementById("newGame");
 newGame.addEventListener("click", () => {
   squares.forEach((elem) => elem.innerHTML = "");
   newBoard();
-  displayMsg('new');
 });
