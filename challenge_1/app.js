5d2019666b454bb7d19619cc1992789d412b616c
NickRashad <nrsternes@gmail.com>
2019-04-05 13:07:52 -0500

    Finish functionality for one game of tictactoe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,9 +4,30 @@ console.log('Yep!');
 // Player's X & O score & startfirst
 // Current Player Turn
 var currentTurn = true;
-var move = () => {
-  return currentTurn ? "X" : "O";
+var board = { 'X': [], 'O': [] };
+var totalWins = { 'X': 0, 'O': 0 };
+var swap = () => currentTurn = ! currentTurn;
+var move = () => currentTurn ? "X" : "O";
+var isWinner = (winner) => totalWins[winner] += 1;
+var canAdd = (position) => {
+  for (let curr in board) {
+    if (board[curr].includes(position)) { return false; };
+  }
+  return true;
 };
+var isWin = (curr) => {
+  let winningMoves = [
+    [1,2,3], [4,5,6], [7,8,9],
+    [1,4,7], [2,5,8], [3,6,9],
+    [1,5,9], [3,5,7]
+  ];
+  for (let arr of winningMoves){
+    if(arr.every(val => board[curr].includes(val))) {
+      return true;
+    };
+  }
+};
+
 // View
 // Player's X & O display indicator and score
 // Session Storage ~ https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
@@ -16,9 +37,12 @@ var move = () => {
 var squares = document.querySelectorAll("th");
 squares.forEach((elem) => {
   elem.addEventListener("click", () => {
-    if (elem.innerHTML.length < 1) {
-      elem.innerHTML= move();
-      currentTurn = ! currentTurn;
+    var curr = move();
+    let pos = Number(elem.className);
+    if (canAdd(pos)) {
+      board[curr].push(pos);
+      elem.innerHTML= curr;
+      isWin(curr) ? isWinner(curr.toString()) : swap();
     }
   });
 });
