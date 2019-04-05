c8050383458fc199c8ba2eef53978ec99b383d8b
NickRashad <nrsternes@gmail.com>
2019-04-05 00:01:52 -0500

    Record X & O on a tic-tac-toe board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,10 +3,25 @@ console.log('Yep!');
 // Model
 // Player's X & O score & startfirst
 // Current Player Turn
-
+var currentTurn = true;
+var move = () => {
+  return currentTurn ? "X" : "O";
+};
 // View
 // Player's X & O display indicator and score
-// 3 X 3 Table
+// Session Storage ~ https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
+
 
 // Controller
-//
+var squares = document.querySelectorAll("th");
+squares.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    if (elem.innerHTML.length < 1) {
+      elem.innerHTML= move();
+      currentTurn = ! currentTurn;
+    }
+  });
+});
+
+// console.log(squares);
+
