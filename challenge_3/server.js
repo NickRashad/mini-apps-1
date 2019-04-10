56e92f895ef59b43344d520c163f748fa6085ae1
NickRashad <nrsternes@gmail.com>
2019-04-10 00:47:46 -0500

    Serve up index.html over express

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
 
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
