712e03cb274cf060c899a26b670bb0c3ea53ab3f
NickRashad <nrsternes@gmail.com>
2019-04-11 15:36:22 -0500

    Begin intialzing React

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class Test extends React.Component {
+  render() {
+    return (
+      <div className="testingpurposes">
+        <h2>To Infinity!!</h2>
+      </div>
+    )
+  }
+}
 
+ReactDOM.render( < Test />, document.getElementById('root'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-
+  res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
 app.post('/', (req, res) => {
