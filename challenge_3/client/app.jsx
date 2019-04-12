124d603b583d4bb99c86c03341ab45e5bbd811d0
NickRashad <nrsternes@gmail.com>
2019-04-11 20:00:44 -0500

    Render react  to index.html

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 class Test extends React.Component {
   render() {
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -4,11 +4,7 @@ const port = 3000;
 const path = require('path');
 const db = require('./db/db.js');
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/', (req, res) => {
 
