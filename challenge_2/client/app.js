60346c82413710c32cab194e3a461d0b0093d800
NickRashad <nrsternes@gmail.com>
2019-04-12 01:05:47 -0500

    Outline for  completion

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,1 +1,9 @@
-console.log('App.js is properly linked to index.html!')
+console.log('App.js is properly linked to index.html!');
+
+// #1 Attach event listner to button
+
+// #2 Refactor form to use FilePicker
+
+// #3 Refactor to single page app with ajax
+
+// #4 Add button functionality to download latest CSV Report
