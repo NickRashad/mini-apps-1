b30ded115ddd3e013bd007de6bbf616a5c5e16f1
NickRashad <nrsternes@gmail.com>
2019-04-13 17:19:28 -0500

    BMR Challenge 3 -Upload checkout info to database upon Checkout

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -8,14 +8,14 @@ const db = require('./db/db.js');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-// app.get('/'), ()
 
 app.post('/', (req, res) => {
-  console.log(req.body, Object.keys(req));
-  db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  let queryColumns = Object.keys(req.body.checkOutData).filter((val, ind) => ind > 0).join(', ');
+  let queryValues = Object.values(req.body.checkOutData).filter((val, ind) => ind > 0);
+  db.query(`INSERT INTO purchase (${queryColumns}) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?);`, queryValues, function (error, results, fields) {
     if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-    res.send({data: results[0].solution});
+    console.log('Upload results:', results);
+    res.send({data: results});
   });
 });
 
