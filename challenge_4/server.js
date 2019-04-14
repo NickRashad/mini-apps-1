0685cec3ac0e20618477a43fe4f455d4e2c354c0
NickRashad <nrsternes@gmail.com>
2019-04-14 18:16:13 -0500

    Initialize application with Webpack Express

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const path = require('path');
+const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('client/dist'))
 
+app.listen(port, () => console.log(`Server is running on port ${port}`));
