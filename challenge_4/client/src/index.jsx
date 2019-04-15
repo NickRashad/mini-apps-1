49ccb11a75e51d9f65991690640e9ac86fa6d5bf
NickRashad <nrsternes@gmail.com>
2019-04-15 00:14:13 -0500

    Toggle colors upon click

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -5,56 +5,65 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      colorChoice: true
     };
+    this.toggleColor = this.toggleColor.bind(this);
+  }
+  toggleColor () {
+    let choice = !this.state.colorChoice;
+    console.log(choice);
+    this.setState({colorChoice: choice});
   }
 
+  toggle (row, column) {
+
+  }
 
   render () {
     return (
       <div className="wrapper">
-        <Square x={0} y={5}/>
-        <Square x={1} y={5}/>
-        <Square x={2} y={5}/>
-        <Square x={3} y={5}/>
-        <Square x={4} y={5}/>
-        <Square x={5} y={5}/>
-        <Square x={6} y={5}/>
-        <Square x={0} y={4}/>
-        <Square x={1} y={4}/>
-        <Square x={2} y={4}/>
-        <Square x={3} y={4}/>
-        <Square x={4} y={4}/>
-        <Square x={5} y={4}/>
-        <Square x={6} y={4}/>
-        <Square x={0} y={3}/>
-        <Square x={1} y={3}/>
-        <Square x={2} y={3}/>
-        <Square x={3} y={3}/>
-        <Square x={4} y={3}/>
-        <Square x={5} y={3}/>
-        <Square x={6} y={3}/>
-        <Square x={0} y={2}/>
-        <Square x={1} y={2}/>
-        <Square x={2} y={2}/>
-        <Square x={3} y={2}/>
-        <Square x={4} y={2}/>
-        <Square x={5} y={2}/>
-        <Square x={6} y={2}/>
-        <Square x={0} y={1}/>
-        <Square x={1} y={1}/>
-        <Square x={2} y={1}/>
-        <Square x={3} y={1}/>
-        <Square x={4} y={1}/>
-        <Square x={5} y={1}/>
-        <Square x={6} y={1}/>
-        <Square x={0} y={0}/>
-        <Square x={1} y={0}/>
-        <Square x={2} y={0}/>
-        <Square x={3} y={0}/>
-        <Square x={4} y={0}/>
-        <Square x={5} y={0}/>
-        <Square x={6} y={0}/>
+        <Square x={0} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={1} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={2} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={3} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={4} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={5} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={6} y={5} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={0} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={1} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={2} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={3} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={4} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={5} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={6} y={4} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={0} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={1} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={2} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={3} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={4} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={5} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={6} y={3} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={0} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={1} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={2} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={3} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={4} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={5} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={6} y={2} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={0} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={1} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={2} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={3} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={4} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={5} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={6} y={1} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={0} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={1} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={2} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={3} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={4} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={5} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
+        <Square x={6} y={0} color={this.state.colorChoice} toggleColor={this.toggleColor} />
       </div>
     );
   }
@@ -64,11 +73,15 @@ class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      color: "white"
     };
+    this.column = this.column.bind(this);
   }
   column (event){
-    console.log(event.target.name)
+    console.log('Column Choice:', event.target.name);
+    let nowColor = this.props.color === true ? "red" : "yellow";
+    this.props.toggleColor();
+    this.setState({ color: nowColor });
   }
   render () {
     const WIDTH = [100,200,300,400,500,600,700][this.props.x];
@@ -76,7 +89,7 @@ class Square extends React.Component {
     return(
       <span className="row" data-x={`"${WIDTH}"`} data-y={`"${HEIGHT}"`}>
         <svg height="100" width="100">
-          <circle cx="40" cy="40" r="40" stroke="black" fill="white" name={`"${this.props.x}"`} onClick={this.column}/>
+          <circle cx="40" cy="40" r="40" stroke="black" fill={`${this.state.color}`} name={`"${this.props.x}"`} onClick={this.column}/>
         </svg>
       </span>
     )
